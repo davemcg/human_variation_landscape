86c953311bd6173d2edbb43625b72a4b349de12b
David McGaughey <mcgaughey@gmail.com>
2016-08-11 10:13:24 -0400

    Runs around 7-10k lines/sec. Creates a sqlite file with the surrounding mafs

--- collapse_loj_sqlite.py
+++ collapse_loj_sqlite.py
@@ -6,13 +6,15 @@ import sqlite3
 
 # create sqlite db
 db = sqlite3.connect('/data/mcgaugheyd/projects/nei/mcgaughey/human_variation_landscape/var_info.sqlite')
+#db = sqlite3.connect(':memory:')
 c = db.cursor()
-#c.execute('''CREATE TABLE variation_landscape (ID TEXT PRIMARY KEY, Num_Var INTEGER, MAFs TEXT, VCF_INFO TEXT)''')
-#db.commit()
+try:
+	c.execute('''CREATE TABLE variation_landscape (ID TEXT PRIMARY KEY, Num_Var INTEGER, MAFs TEXT, VCF_INFO TEXT)''')
+except:
+	print('Table variation_landscape already exists')#db.commit()
 
 # read from stdin
 for line in fileinput.input():	
-
 	line = line.split('\t')	
 
 	key = line[3]
@@ -28,19 +30,22 @@ for line in fileinput.input():
 	# lump together
 	vcf_info = str(chr) + '\t' + str(pos) + '\t' + id + '\t' + ref + '\t' + alt + '\t.\t.'
 
-	regex=re.compile(r'.*MAF=0\.\d+', re.I) # regex pattern to find the maf
+	regex=re.compile(r'MAF=0\.\d+', re.I) # regex pattern to find the maf
 
 	if key == window_key: # skip cases where the variants are the same 
 		continue
 	maf = 0 # to cover when no maf is present
 	if regex.search(info):
 		maf = [float(m.group().split('=')[1]) for section in info.split(';') for m in [regex.search(section)] if m][0]
-	print(key)
-	data=c.execute('''SELECT * FROM variation_landscape WHERE ID=? ''',(key,)).fetchone()
-	print(data)
-	if data is None:
+	try:
 		c.execute('INSERT INTO variation_landscape (ID, Num_Var, MAFs, VCF_INFO) VALUES (?,?,?,?)', \
 			(key, 1, maf, vcf_info))
+	except:
+		old_data = c.execute('''SELECT * FROM variation_landscape WHERE ID=? ''',(key,)).fetchone()
+		new_mafs = old_data[2] + ',' + str(maf)
+		c.execute('''UPDATE variation_landscape SET MAFs=? WHERE ID=?''',(new_mafs,key))
+
+db.commit()
 #	else:
 #		num_var, the_mafs, vcf_info = data[key]
 #		num_var += 1
