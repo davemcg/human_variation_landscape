ad0b85f8011afbf6d2ca47549410f1d31d2a3619
David McGaughey <mcgaughey@gmail.com>
2016-08-11 11:22:15 -0400

    now adds distance from each surroudning variant to the primary variant

--- loj_processor.py
+++ loj_processor.py
@@ -9,9 +9,10 @@ db = sqlite3.connect('/data/mcgaugheyd/projects/nei/mcgaughey/human_variation_la
 #db = sqlite3.connect(':memory:')
 c = db.cursor()
 try:
-	c.execute('''CREATE TABLE variation_landscape (ID TEXT PRIMARY KEY, Num_Var INTEGER, MAFs TEXT, VCF_INFO TEXT)''')
+	c.execute('''CREATE TABLE variation_landscape (ID TEXT PRIMARY KEY, \
+				Num_Var INTEGER, MAFs TEXT, DISTANCEs TEXT, VCF_INFO TEXT)''')
 except:
-	print('Table variation_landscape already exists')#db.commit()
+	print('Table variation_landscape already exists')
 
 # read from stdin
 for line in fileinput.input():	
@@ -23,27 +24,33 @@ for line in fileinput.input():
 
 	# grab info to build vcf at the end
 	chr = line[4]
-	pos = line[5]
-	id = line[6]
+	pos = int( (int(line[1]) + int(line[2])) / 2)
+	id = line[3]
 	ref = line[7]
 	alt = line[8]
 	# lump together
 	vcf_info = str(chr) + '\t' + str(pos) + '\t' + id + '\t' + ref + '\t' + alt + '\t.\t.'
 
-	regex=re.compile(r'MAF=0\.\d+', re.I) # regex pattern to find the maf
+	# calculate distance of landscape variant to primary variant
+	orig_pos = int( (int(line[1]) + int(line[2])) / 2) # find center to get original position
+	distance = orig_pos - int(line[5])
 
 	if key == window_key: # skip cases where the variants are the same 
 		continue
+	
 	maf = 0 # to cover when no maf is present
+	regex=re.compile(r'MAF=0\.\d+', re.I) # regex pattern to find the maf
 	if regex.search(info):
 		maf = [float(m.group().split('=')[1]) for section in info.split(';') for m in [regex.search(section)] if m][0]
 	try:
-		c.execute('INSERT INTO variation_landscape (ID, Num_Var, MAFs, VCF_INFO) VALUES (?,?,?,?)', \
-			(key, 1, maf, vcf_info))
+		c.execute('INSERT INTO variation_landscape (ID, Num_Var, MAFs, DISTANCEs, VCF_INFO) \
+			VALUES (?,?,?,?,?)', \
+			(key, 1, maf, distance, vcf_info))
 	except:
 		old_data = c.execute('''SELECT * FROM variation_landscape WHERE ID=? ''',(key,)).fetchone()
 		new_mafs = old_data[2] + ',' + str(maf)
-		c.execute('''UPDATE variation_landscape SET MAFs=? WHERE ID=?''',(new_mafs,key))
+		new_distances = old_data[3] + ',' + str(distance)
+		c.execute('''UPDATE variation_landscape SET MAFs=?,DISTANCEs=? WHERE ID=?''',(new_mafs,new_distances,key))
 
 db.commit()
 #	else:
