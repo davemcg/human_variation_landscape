41326e2aeeb10e7f6540489f905e20d8ac20083f
David McGaughey <mcgaughey@gmail.com>
2016-08-17 15:51:12 -0400

    not used

--- 02_create_variation_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zcat /data/mcgaugheyd/projects/nei/mcgaughey/human_variation_landscape/20160701_ensembl_homo_sapiens_variation.100bp_window.loj.dat.gz | /home/mcgaugheyd/git/human_variation_landscape/./loj_processor.py -
