e3662b997d748f33cbba37657609dc193722913a
David McGaughey <mcgaughey@gmail.com>
2016-09-07 13:43:33 -0400

    added cadd via dbnSFP

--- scripts/run_VEP.sh
+++ scripts/run_VEP.sh
@@ -22,9 +22,9 @@ if [ "$genome" == "GRCh38" ] || [ "$genome" == "GRCh37" ] && [ "$file_type" == "
 	--maf_1kg \
 	--maf_esp \
 	--maf_exac \
-	--plugin CADD \
 	--domains \
 	--total_length \
+	--plugin dbNSFP,/data/mcgaugheyd/genomes/GRCh38/dbNSFP3.2/dbNSFP.s2.gz,CADD_raw,CADD_raw_rankscore,CADD_phred \
 	--tab --force_overwrite --fork $cores
 
 elif [ "$genome" == "GRCh38" ] || [ "$genome" == "GRCh37" ] && [ "$file_type" == "vcf" ]; then
@@ -41,9 +41,9 @@ elif [ "$genome" == "GRCh38" ] || [ "$genome" == "GRCh37" ] && [ "$file_type" ==
 	--maf_1kg \
 	--maf_esp \
 	--maf_exac \
-	--plugin CADD \
 	--domains \
 	--total_length \
+	--plugin dbNSFP,/data/mcgaugheyd/genomes/GRCh38/dbNSFP3.2/dbNSFP.s2.gz,CADD_raw,CADD_raw_rankscore,CADD_phred \
 	--vcf --force_overwrite --fork $cores
 
 else
